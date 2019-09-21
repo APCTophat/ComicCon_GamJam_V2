1291f97938de25ded37565d6635d0f06b8f64e29
1851866 <1851866@users.noreply.github.com>
2019-09-21 15:34:42 +0200

    Added generated plane

--- ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/SpawnGrid.cs
+++ ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/SpawnGrid.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SpawnGrid : MonoBehaviour
 {
+    public GameObject LevelPiece;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 10; i++)
+        {
+            float xpos = i;
+            for (int j = 0; j < 10; j++)
+            {
+                float zpos = j;
+                Instantiate(LevelPiece, new Vector3(xpos,-0.5f,zpos), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
