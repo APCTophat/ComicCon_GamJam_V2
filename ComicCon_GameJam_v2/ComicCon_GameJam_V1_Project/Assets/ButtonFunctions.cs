c31ed923f894f36ca7da550f2bee00c8576a1dfc
1851866 <1851866@users.noreply.github.com>
2019-09-22 15:04:12 +0200

    updated UI

--- ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/ButtonFunctions.cs
+++ ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/ButtonFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
@@ -15,4 +16,26 @@ public class ButtonFunctions : MonoBehaviour
     {
         
     }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene(4);
+    }
+
 }
