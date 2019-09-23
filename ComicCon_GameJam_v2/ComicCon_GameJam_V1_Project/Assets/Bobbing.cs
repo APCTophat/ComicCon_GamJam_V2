137a1ecc9a4bd0ee1c02527bf74c08805f12030c
1851866 <1851866@users.noreply.github.com>
2019-09-23 12:00:12 +0200

    added instruction

---
+++ ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/Bobbing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobbing : MonoBehaviour
+{
+    private float PositionY;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PositionY = Mathf.PingPong(Time.time, 0.5f);
+        transform.localPosition = new Vector3(transform.localPosition.x, PositionY, transform.localPosition.z);
+    }
+}
