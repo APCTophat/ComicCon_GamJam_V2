37d82154e64cb792b91d73813b32f560de302e2a
1851866 <1851866@users.noreply.github.com>
2019-09-23 12:26:08 +0200

    fixed level order

--- ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/ButtonFunctions.cs
+++ ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/ButtonFunctions.cs
@@ -19,23 +19,23 @@ public class ButtonFunctions : MonoBehaviour
 
     public void LoadLevel1()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void LoadLevel2()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
     public void LoadLevel3()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
     public void LoadLevel4()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
     public void LoadLevel5()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(6);
     }
     public void LoadLevel6()
     {
