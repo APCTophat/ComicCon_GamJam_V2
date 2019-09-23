4b69d953686a7debd82abf0f885d3199373400de
APCTophat <49924900+APCTophat@users.noreply.github.com>
2019-09-23 10:53:40 +0200

    Menu Done

---
+++ ComicCon_GameJam_v2/ComicCon_GameJam_V1_Project/Assets/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public string Scene;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+
+}
